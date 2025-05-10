9cbd3055a7f89e343f4314e42edc1cbcd2ba858f
cwaller33 <59543955+cwaller33@users.noreply.github.com>
2025-05-09 20:58:42 -0400

    Test fix

--- src/app/components/MainTopicTitle.tsx
+++ src/app/components/MainTopicTitle.tsx
@@ -1,4 +1,4 @@
-// import styles from "./styles/MainTopicTitle.module.css";
+import styles from "./styles/MainTopicTitle.module.css";
 
 export default function MainTopicTitle() {
   return <h1 className={styles.mainTopicTitle}> Discovery Feed </h1>;
